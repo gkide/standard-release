983473498b44d13b20b9c081254a76cd3f715b8f
Charlie WONG <1213charlie@163.com>
2018-12-07 20:03:26 +0800

    test: standard-release --init

--- test.js
+++ test.js
@@ -5,8 +5,18 @@ const path = require('path');
 const stream = require('stream');
 const semver = require('semver');
 const shell = require('shelljs');
+const chai = require('chai');
 
-const standardRelease = require('./helper').standardRelease;
+const cliPath = path.resolve(__dirname, 'bin', 'cli.js');
+
+function standardRelease(argString) {
+  return shell.exec('node ' + cliPath + (argString != null ? ' ' + argString : ''))
+}
+
+function gitTag(tag, msg) {
+    shell.exec('git tag -a ' + tag + ' -m "' + msg + '"');
+    shell.exec('git tag -a v1.0.0 -m "my awesome first release"');
+}
 
 function gitBranch(branch) {
     shell.exec('git branch ' + branch);
@@ -28,30 +38,43 @@ function getPackageVersion() {
     return JSON.parse(fs.readFileSync('package.json', 'utf-8')).version;
 }
 
-function tmpDirInit() {
-    shell.rm('-rf', 'tmp');
-    shell.config.silent = true;
-    shell.mkdir('tmp');
-    shell.cd('tmp');
-    shell.exec('git init');
-    gitCommit('Init commit');
-}
+// Suppresses all command output if true, except for echo() calls.
+shell.config.silent = true;
 
-function tmpDirClean() {
-    shell.cd('../');
-    shell.rm('-rf', 'tmp');
-}
-
-describe('cli', function() {
-    beforeEach(tmpDirInit);
-    afterEach(tmpDirClean);
-
-    describe('CHANGELOG.md does not exist', function() {
-        it('populates changelog with commits since last tag by default', function() {
-            gitCommit('feat: first commit');
-            shell.exec('git tag -a v1.0.0 -m "my awesome first release"');
-            gitCommit('fix: patch release');
-        })
-
+describe('standard-release --init', function() {
+    beforeEach(function() {
+        shell.rm('-rf', 'tmp');
+        shell.mkdir('tmp');
+        shell.cd('tmp');
+    });
+    afterEach(function() {
+        shell.cd('../');
+        shell.rm('-rf', 'tmp');
+    });
+    
+    it('it is not a git repo', function() {
+        let ret = standardRelease('-i');
+        chai.expect(ret.code).to.equal(1);
+        chai.expect(ret.stdout).to.empty;
+        const emsg = path.resolve(__dirname, 'tmp', '.git') + "'\n";
+        chai.expect(ret.stderr).to.equal("ERROR: Do not find '" + emsg);
     })
+    it('init a git repo, check again', function() {
+        shell.exec('git init');
+        gitCommit('Init commit');
+        let ret = standardRelease('-i');
+        chai.expect(ret.code).to.equal(0);
+        chai.expect(ret.stdout).to.empty;
+        chai.expect(ret.stderr).to.empty;
+    })
+    it('git repo not exist', function() {
+        shell.exec('git init');
+        gitCommit('Init commit');
+        let ret = standardRelease('-i x');
+        chai.expect(ret.code).to.equal(1);
+        chai.expect(ret.stdout).to.empty;
+        const emsg = path.resolve(__dirname, 'tmp', 'x') + "'\n";
+        chai.expect(ret.stderr).to.equal("ERROR: Do not exist '" + emsg);
+    })
+
 })
