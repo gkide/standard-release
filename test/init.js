20273bbca24c2063961087c6ce24fd5ba576c7f3
Charlie WONG <1213charlie@163.com>
2018-12-20 23:27:31 +0800

    fix: stupid init bugs

--- test/init.js
+++ test/init.js
@@ -5,16 +5,19 @@ const chai = require('chai');
 const shell = require('shelljs');
 
 function runTesting(standardRelease) {
-    const workingDirectory = path.resolve(__dirname, '..', 'tmp');
+    // out of the source tree
+    const workingDirectory = path.resolve(__dirname, '..', '..');
 
     describe('standard-release --init', function() {
         beforeEach(function() {
+            shell.cd(workingDirectory);
             shell.rm('-rf', 'tmp');
             shell.mkdir('tmp');
             shell.cd('tmp');
         });
+
         afterEach(function() {
-            shell.cd('../');
+            shell.cd('..');
             shell.rm('-rf', 'tmp');
         });
 
@@ -22,12 +25,12 @@ function runTesting(standardRelease) {
             let ret = standardRelease('-i');
             chai.expect(ret.code).to.equal(1);
             chai.expect(ret.stdout).to.empty;
-            const emsg = path.resolve(workingDirectory, '.git') + "'\n";
-            chai.expect(ret.stderr).to.equal("ERROR: Do not find '" + emsg);
+            const emsg = path.resolve(workingDirectory, 'tmp') + "'\n";
+            chai.expect(ret.stderr).to.equal("ERROR: Do not git repo: '" + emsg);
         });
 
         it('init a git repo, check again', function() {
-            shell.exec('mkdir .git');
+            shell.exec('git init');
             let ret = standardRelease('-i');
             chai.expect(ret.code).to.equal(0);
             chai.expect(ret.stdout).to.empty;
@@ -35,12 +38,29 @@ function runTesting(standardRelease) {
         });
 
         it('git repo not exist', function() {
-            shell.exec('mkdir .git');
             let ret = standardRelease('-i x');
             chai.expect(ret.code).to.equal(1);
             chai.expect(ret.stdout).to.empty;
-            const emsg = path.resolve(workingDirectory, 'x') + "'\n";
+            const emsg = path.resolve(workingDirectory, 'tmp', 'x') + "'\n";
             chai.expect(ret.stderr).to.equal("ERROR: Do not exist '" + emsg);
+        });
+
+        it('directory existance check first', function() {
+            shell.exec('git init');
+            let ret = standardRelease('-i x');
+            chai.expect(ret.code).to.equal(1);
+            chai.expect(ret.stdout).to.empty;
+            const emsg = path.resolve(workingDirectory, 'tmp', 'x') + "'\n";
+            chai.expect(ret.stderr).to.equal("ERROR: Do not exist '" + emsg);
+        });
+
+        it('can find correct git repo path', function() {
+            shell.exec('git init');
+            shell.mkdir('x');
+            let ret = standardRelease('-i x');
+            chai.expect(ret.code).to.equal(0);
+            chai.expect(ret.stdout).to.empty;
+            chai.expect(ret.stderr).to.empty;
         });
     });
 }
