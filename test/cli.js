f0dd22cc118712a9a097e5ef24e782948521119c
Charlie WONG <1213charlie@163.com>
2019-01-24 00:55:50 +0800

    test: rename & style only

--- test/cli.js
+++ test/cli.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const path = require('path');
 const stream = require('stream');
 const semver = require('semver');
@@ -21,21 +20,21 @@ function getPackageVersion() {
 shell.config.silent = true;
 
 // package: git semver tags
-const myGitSuite = path.resolve(__dirname, 'package', 'myGit.js');
-require(myGitSuite).runTesting();
+const myGit = path.resolve(__dirname, 'package', 'myGit.js');
+require(myGit).runTesting();
 
 // standard-release -i
-const initSuite = path.resolve(__dirname, 'init.js');
-require(initSuite).runTesting(standardRelease);
-
-// standard-release -m, use default configuration commit rules 
-const msgDefaultSuite = path.resolve(__dirname, 'commit-rules-def.js');
-require(msgDefaultSuite).runTesting(standardRelease);
-
-// standard-release -m, use custom configuration commit rules
-const msgCustomSuite = path.resolve(__dirname, 'commit-rules-usr.js');
-require(msgCustomSuite).runTesting(standardRelease);
+const init = path.resolve(__dirname, 'init.js');
+require(init).runTesting(standardRelease);
 
 // standard-release --is-semver
-const mySemVerSuite = path.resolve(__dirname, 'mySemVer.js');
-require(mySemVerSuite).runTesting(standardRelease);
+const isSemVer = path.resolve(__dirname, 'is-semver.js');
+require(isSemVer).runTesting(standardRelease);
+
+// standard-release -m, use default commit rules
+const defCommitRules = path.resolve(__dirname, 'commit-rules-def.js');
+require(defCommitRules).runTesting(standardRelease);
+
+// standard-release -m, use custom commit rules
+const usrCommitRules = path.resolve(__dirname, 'commit-rules-usr.js');
+require(usrCommitRules).runTesting(standardRelease);
